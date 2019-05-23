dd59c990261eefbb67cac2518d8af5cc56a95c0a
usefulparade <singingland@gmail.com>
2019-05-23 16:10:13 -0400

    three tests working

--- js/google-get.js
+++ js/google-get.js
@@ -13,12 +13,29 @@ function init (){
                     populateContent(data);
                     //    archive = loadJSON(data);
                    },
-                   simpleSheet: true } )
+                   simpleSheet: true } );
 
 }
 
 
 function populateContent(sheet){
     console.log(sheet);
+    
+    var archiveBlock = document.getElementById("archive");
+    var audioBlock = document.getElementById("audio");
+
+    var imgTest = createImg("https://drive.google.com/uc?id=" + sheet[2].CONTENT);
+    imgTest.addClass("archiveImage");
+    imgTest.parent(archiveBlock);
+
+    // var vidTest = createVideo("https://drive.google.com/uc?export=download&id=" + sheet[0].CONTENT);
+    var vidTest = createDiv("<iframe src='" + sheet[0].CONTENT + "/preview' width='640' height='480'></iframe>");
+    vidTest.addClass("archiveVideo");
+    vidTest.parent(archiveBlock);
+
+    var audioTest = createDiv("<iframe src='" + sheet[1].CONTENT + "/preview' width='640' height='480'></iframe>");
+    // var audioTest = createAudio("<iframe src='" + sheet[1].CONTENT + "/preview'></iframe>");
+    // audioTest.addClass("archiveAudio");
+    audioTest.parent(audioBlock);
 }
 
