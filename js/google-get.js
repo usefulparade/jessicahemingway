c329271513cb3f22c015cfa4267f6f9785caf18c
usefulparade <singingland@gmail.com>
2019-06-29 22:44:09 -0400

    full working prototype! still pretty buggy on the phone, also audio posts are hard to interact with

--- js/google-get.js
+++ js/google-get.js
@@ -2,6 +2,8 @@ var spreadsheetURL = "https://docs.google.com/spreadsheets/d/1mJ3ccpWe6SR6FJNw1u
 
 var posts = [];
 
+var months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+
 function setup(){
     init();
 }
@@ -9,11 +11,8 @@ function setup(){
 function init (){
     Tabletop.init( { key: spreadsheetURL,
                    callback: function(data, tabletop) { 
-                       
-                    //    console.log(dataGlobal);
-                    // console.log(data);
+                    // when we get the data, call our function to fill the site :)
                     populateContent(data);
-                    //    archive = loadJSON(data);
                    },
                    simpleSheet: true } );
 
@@ -23,9 +22,30 @@ function init (){
 function populateContent(sheet){
     console.log(sheet);
 
+    for (l=0;l<sheet.length;l++){
+        if (sheet[l].DATE != ""){
+
+        } else {
+            sheet[l].DATE = sheet[l].TIMESTAMP;
+        }
+    }
+
+    sheet.sort(function(a, b){
+        var c = new Date(a.DATE);
+        var d = new Date(b.DATE);
+        return c-d;
+    });
+
+    sheet.reverse();
+
     var dates = [];
     for (j=0;j<sheet.length;j++){
-        var d = new Date(sheet[j].DATE);
+        var d;
+        if (sheet[j].DATE != ""){
+            d = new Date(sheet[j].DATE);
+        } else {
+            d = new Date(sheet[j].TIMESTAMP);
+        }
         dates[j] = d;
     }
 
@@ -41,9 +61,7 @@ function populateContent(sheet){
         var r3 = random(-30, 30);
 
         var thisDate = createDiv().addClass('blockDate');
-        // var thisMonth = createElement("h4", ("0" + dates[i].getMonth()).slice(-2)).addClass('month');
-        // var thisDay = createElement("h4", ("0" + dates[i].getDate()).slice(-2)).addClass('day');
-        var thisMonth = createElement("h4", (dates[i].getMonth()) + 1).addClass('month');
+        var thisMonth = createElement("h4", months[(dates[i].getMonth())]).addClass('month');
         var thisDay = createElement("h4", (dates[i].getDate())).addClass('day');
         var thisYear = createElement("h4", ("" + dates[i].getFullYear()).slice(-2)).addClass('year');
 
@@ -59,7 +77,7 @@ function populateContent(sheet){
         thisDay.parent(thisDate);
         thisYear.parent(thisDate);
 
-        var newBlock = new ArchiveBlock(sheet[i].TYPE, thisDate, sheet[i].TITLE, sheet[i].CONTENT, sheet[i].CAPTION);
+        var newBlock = new ArchiveBlock(sheet[i].TYPE, thisDate, sheet[i].TITLE, sheet[i].MEDIA, sheet[i].TEXT, sheet[i].EMBED, sheet[i].CAPTION, sheet[i].LINK);
         posts.push(newBlock);
     }
 
@@ -67,27 +85,6 @@ function populateContent(sheet){
         posts[k].createBlock();
         posts[k].postBlock();
     }
-
-    // var imgTest = createImg("https://drive.google.com/uc?id=" + sheet[2].CONTENT);
-    // var imgLink = createA("www.google.com", "hi");
-    // var imgAspect = createDiv().addClass('imageAspect');
-    // var imgBlock = createDiv().addClass('imageBlock');
-    
-    // imgTest.parent(imgAspect);
-    // imgAspect.parent(imgBlock);
-    // imgBlock.parent(archive);
-
-    // var vidTest = createDiv("<iframe src='" + sheet[0].CONTENT + "/preview' width='640' height='480'></iframe>");
-
-    // var vidAspect = createDiv().addClass("four-by-three");
-    // var vidBlock = createDiv().addClass("videoBlock");
-    // vidTest.parent(vidAspect);
-    // vidAspect.parent(vidBlock);
-    // vidBlock.parent(archive);
-
-    // var audioTest = createDiv("<iframe src='" + sheet[1].CONTENT + "/preview'></iframe>");
-    // audioTest.addClass("audioBlock");
-    // audioTest.parent(archive);
 }
 
 function openArchive(){
@@ -104,18 +101,39 @@ function openGarden(){
     document.getElementById("column").style = "background-color: var(--color-six);";
 }
 
-function ArchiveBlock(type, date, title, content, caption){
+function ArchiveBlock(type, date, title, content, text, embed, caption, link){
     this.type = type;
     this.date = date;
     this.title = title;
     this.blockContent = createDiv().addClass('blockContent');
     this.blockCaption = createDiv().addClass('blockCaption');
-    this.innerContent = content;
+    this.innerMedia = content;
+    this.innerText = text;
+    this.embed = embed;
     this.caption = caption;
-    this.innerContentID = this.innerContent.split('=');
+    this.hasCaption = false;
+    this.link = link;
+    this.hasLink = false;
 
     this.createBlock = function(){
 
+        if (this.innerMedia != ""){
+            this.innerMediaID = this.innerMedia.split('=');
+        }
+
+        if (this.link != ""){
+            this.hasLink = true;
+        } else {
+            this.hasLink = false;
+        }
+
+        if (this.caption != ""){
+            this.hasCaption = true;
+        } else {
+            this.hasCaption = false;
+        }
+
+        //MAKE TITLE, RANDOMIZE ANIMATION
         if (this.title != ""){
             this.titleBlock = createElement('h3', this.title).addClass('titleBlock');
             this.titleBlock.style('animation-delay', random(0,3) + 's');
@@ -124,23 +142,34 @@ function ArchiveBlock(type, date, title, content, caption){
             this.titleBlock = createDiv().style('display', 'none').addClass('titleBlock');
         }
 
+        //MAKE CONTENT BLOCKS!
+
         if (this.type == 'video'){
             this.block = createDiv().addClass('archiveBlock videoBlock');
-            // this.link = createA(this.innerContent, this.caption, "_blank");
-            this.vid = createDiv("<iframe src='https://drive.google.com/file/d/" + this.innerContentID[1] + "/preview'></iframe>");
+            this.vid = createDiv("<iframe src='https://drive.google.com/file/d/" + this.innerMediaID[1] + "/preview'></iframe>");
             this.vid.parent(this.blockContent);
 
             // ADD CAPTION
-            this.captionContent = createElement('h5', this.caption);
-            this.captionContent.parent(this.blockCaption);
+            if (!this.hasCaption){
+
+            } else {
+                if (!this.hasLink) {
+                    this.captionH5 = createElement('h5', this.caption);
+                } else {
+                    this.captionH5 = createElement('h5');
+                    this.captionA = createA(this.link, this.caption, '_blank');
+                    this.captionA.parent(this.captionH5);
+                }
+                this.captionH5.parent(this.blockCaption);
+            }
             
         } else if (this.type == 'audio'){
             //random number for audio block rotation
             var r4 = random(70, 88);
 
             this.block = createDiv().addClass('archiveBlock audioBlock');
-            // this.link = createA(this.innerContent, this.caption, "_blank");
-            this.audio = createDiv("<iframe src='https://drive.google.com/file/d/" + this.innerContentID[1] + "/preview'></iframe>");
+            // this.link = createA(this.innerMedia, this.caption, "_blank");
+            this.audio = createDiv("<iframe src='https://drive.google.com/file/d/" + this.innerMediaID[1] + "/preview'></iframe>");
             this.audioWrapper = createDiv().addClass('audioWrapper');
 
             this.audioWrapper.style('transform', 'rotate(' + r4 + 'deg)');
@@ -148,25 +177,67 @@ function ArchiveBlock(type, date, title, content, caption){
             this.audio.parent(this.audioWrapper);
 
             // ADD CAPTION
-            this.captionContent = createElement('h5', this.caption);
-            this.captionContent.parent(this.blockCaption);
+            if (!this.hasCaption){
+
+            } else {
+                if (!this.hasLink) {
+                    this.captionH5 = createElement('h5', this.caption);
+                } else {
+                    this.captionH5 = createElement('h5');
+                    this.captionA = createA(this.link, this.caption, '_blank');
+                    this.captionA.parent(this.captionH5);
+                }
+                this.captionH5.parent(this.blockCaption);
+            }
 
         } else if (this.type == 'image'){
             this.block = createDiv().addClass('archiveBlock imgBlock');
-            this.imgBlock = createImg('https://drive.google.com/uc?export=view&id=' + this.innerContentID[1]);
+            this.imgBlock = createImg('https://drive.google.com/uc?export=view&id=' + this.innerMediaID[1]);
             this.imgBlock.parent(this.blockContent);
-            // this.link = createA(this.innerContent, this.caption, "_blank");
-            // this.link.parent(this.blockContent);
 
             // ADD CAPTION
-            this.captionContent = createElement('h5', this.caption);
-            this.captionContent.parent(this.blockCaption);
-        } else if (this.type == 'text'){
-            this.block = createDiv().addClass('archiveBlock textBlock');
-            this.text = createP(this.innerContent);
-            this.text.parent(this.blockContent);
-        } else {
+            if (!this.hasCaption){
 
+            } else {
+                if (!this.hasLink) {
+                    this.captionH5 = createElement('h5', this.caption);
+                } else {
+                    this.captionH5 = createElement('h5');
+                    this.captionA = createA(this.link, this.caption, '_blank');
+                    this.captionA.parent(this.captionH5);
+                }
+                this.captionH5.parent(this.blockCaption);
+            }
+        } else if (this.type == 'text'){
+            var r5 = random(0, -10);
+            this.block = createDiv().addClass('archiveBlock textBlock');
+            
+            if (!this.hasLink){
+                this.text = createP(this.innerText);
+            } else {
+                this.text = createP();
+                this.A = createA(this.link, this.innerText, '_blank');
+                this.A.parent(this.text);
+            }
+            this.text.parent(this.blockContent);
+        } else if (this.type == 'embed'){
+            this.block = createDiv().addClass('archiveBlock videoBlock');
+            this.vid = createDiv(this.embed);
+            this.vid.parent(this.blockContent);
+
+            // ADD CAPTION
+            if (!this.hasCaption){
+
+            } else {
+                if (!this.hasLink) {
+                    this.captionH5 = createElement('h5', this.caption);
+                } else {
+                    this.captionH5 = createElement('h5');
+                    this.captionA = createA(this.link, this.caption, '_blank');
+                    this.captionA.parent(this.captionH5);
+                }
+                this.captionH5.parent(this.blockCaption);
+            }
         }
     };
 
@@ -174,7 +245,9 @@ function ArchiveBlock(type, date, title, content, caption){
         this.titleBlock.parent(this.block);
         this.date.parent(this.block);
         this.blockContent.parent(this.block);
-        this.blockCaption.parent(this.block);
+        if (this.hasCaption){
+            this.blockCaption.parent(this.block);
+        }
         this.block.parent(archive);
     };
 }
